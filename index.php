739de854d20ae237d1fc3e67b5ac849944c101fa
fati-zahra <81538798+fati-zahra@users.noreply.github.com>
2022-09-30 16:10:31 +0100

    Update index.php

--- index.php
+++ index.php
@@ -86,8 +86,8 @@ session_start();
            <footer class="footer"> 
                <div class="container">
                <center>
-                   <p>Copyright &copy <a href="https://projectworlds.in">Projectworlds</a> Store. All Rights Reserved.</p>
-                   <p>This website is developed by Yugesh Verma</p>
+                  
+                   <p>RJP store</p>
                </center>
                </div>
            </footer>
